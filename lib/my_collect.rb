61c770040a33c868a910250087bd8b23d94f99be
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-12-20 15:32:48 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(arr)
+    new_array = [] #set up to store a new array from arr
+    count = 0  # index number
+    while count < arr.size
+    new_array << yield(arr[count])
+        count += 1
+    end # block ends
 
+    return new_array #returns modified collection new array
+
+end # def method closed  
+
+my_collect { puts new_array}
 
